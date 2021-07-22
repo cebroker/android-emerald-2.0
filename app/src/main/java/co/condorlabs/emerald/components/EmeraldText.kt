088fc7991769b065bb5f02349403a9d03328dbfd
ARIAS <bsarias@condorlabs.io>
2021-07-22 09:04:40 -0500

    adding optional parameters to EmeraldText

--- app/src/main/java/co/condorlabs/emerald/components/EmeraldText.kt
+++ app/src/main/java/co/condorlabs/emerald/components/EmeraldText.kt
@@ -3,9 +3,18 @@ package co.condorlabs.emerald.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import co.condorlabs.emerald.theme.EmeraldColors
 import co.condorlabs.emerald.theme.EmeraldTheme
 import co.condorlabs.emerald.theme.EmeraldTypography
@@ -30,11 +39,40 @@ fun PreviewEmeraldText() {
 }
 
 @Composable
-fun EmeraldText(text: String, style: EmeraldTextStyle) {
+fun EmeraldText(
+    text: String,
+    style: EmeraldTextStyle,
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+) {
     Text(
         text = text,
         style = style.getTextStyle(),
         color = style.getColorText(),
+        modifier = modifier,
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        onTextLayout = onTextLayout,
     )
 }
 
