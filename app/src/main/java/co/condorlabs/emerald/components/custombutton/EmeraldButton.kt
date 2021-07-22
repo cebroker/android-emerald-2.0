2fcfc9ecf17cfa64b4c933ba5664b1a4f01652b1
Sebastian Alvarez <>
2021-07-22 12:36:51 -0500

    MA-300: - Add ripple color theme.

--- app/src/main/java/co/condorlabs/emerald/components/custombutton/EmeraldButton.kt
+++ app/src/main/java/co/condorlabs/emerald/components/custombutton/EmeraldButton.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.*
+import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import co.condorlabs.emerald.theme.RippleColorTheme
 
 @Composable
 fun EmeraldButton(
@@ -25,31 +28,33 @@ fun EmeraldButton(
     val emeraldButtonStyle: EmeraldButtonStyle =
         EmeraldButtonStyleFactory().getEmeraldButtonStyle(emeraldButtonType)
 
-    Button(
-        onClick = clickAction,
-        border = emeraldButtonStyle.strokeWidth,
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = emeraldButtonStyle.backgroundColor
-        ),
-        modifier = modifier,
-        enabled = enabled,
-        interactionSource = interactionSource,
-        elevation = elevation,
-        shape = shape,
-        contentPadding = contentPadding
-    ) {
-        when(buttonState) {
-            ButtonState.Normal -> {
-                Text(
-                    text = text,
-                    color = emeraldButtonStyle.textColor
-                )
-            }
-            ButtonState.Loading -> {
-                CircularProgressIndicator(
-                    modifier = Modifier.wrapContentHeight(),
-                    color = emeraldButtonStyle.textColor,
-                )
+    CompositionLocalProvider(LocalRippleTheme provides RippleColorTheme(emeraldButtonStyle.rippleColor)) {
+        Button(
+            onClick = clickAction,
+            border = emeraldButtonStyle.strokeWidth,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = emeraldButtonStyle.backgroundColor
+            ),
+            modifier = modifier,
+            enabled = enabled,
+            interactionSource = interactionSource,
+            elevation = elevation,
+            shape = shape,
+            contentPadding = contentPadding
+        ) {
+            when (buttonState) {
+                ButtonState.Normal -> {
+                    Text(
+                        text = text,
+                        color = emeraldButtonStyle.textColor
+                    )
+                }
+                ButtonState.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.wrapContentHeight(),
+                        color = emeraldButtonStyle.textColor,
+                    )
+                }
             }
         }
     }
