b60511d458084ed24ccddcb436828eb3ce0d07c1
Sebastian Alvarez <>
2021-07-28 16:53:47 -0500

    MA-300: - Remove Box container and put default size to circular progress bar

--- app/src/main/java/co/condorlabs/emerald/theme/Dimens.kt
+++ app/src/main/java/co/condorlabs/emerald/theme/Dimens.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.unit.dp
 object EmeraldDimens {
     val PaddingErrorMessageTextField = 16.dp
     val DefaultWidthToBorderStroke = 1.dp
-    val DefaultReducePadding = 10.dp
+    val DefaultSizeCircularProgress = 25.dp
     val DefaultProgressStrokeWidth = 8.dp
     val DefaultElevation = 2.dp
     val DefaultPressedElevation = 8.dp
