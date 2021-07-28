b71243bd26b8f600cad8890473cd2ac0b7d40bf9
Sebastian Alvarez <>
2021-07-28 00:23:08 -0500

    Demo: - Add paddings and lorem ipsum text to labels

--- demo/src/main/java/co/condorlabs/demo/screens/samples/TextFieldScreen.kt
+++ demo/src/main/java/co/condorlabs/demo/screens/samples/TextFieldScreen.kt
@@ -1,10 +1,13 @@
 package co.condorlabs.demo.screens.samples
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import co.condorlabs.emerald.components.textfield.EmeraldTextField
 import co.condorlabs.emerald.components.textfield.EmeraldTextFieldState
 
@@ -33,28 +36,32 @@ fun TextFieldScreenSample() {
     val onValueChangedError = { text: TextFieldValue ->
         textStateError.value = EmeraldTextFieldState(text = text)
         if (text.text.length > 5) {
-            textStateError.value = textStateError.value.copy(text = text, error = "This is an error message")
+            textStateError.value =
+                textStateError.value.copy(text = text, error = "This is an error message")
         }
     }
 
-    Column {
+    Column(modifier = Modifier.padding(10.dp)) {
         EmeraldTextField(
             state = textStateEmpty.value,
             onValueChange = onValueChangedEmpty,
             label = "Normal",
+            modifier = Modifier.padding(top = 10.dp)
         )
         EmeraldTextField(
             state = textStateCorrect.value,
             onValueChange = onValueChangedCorrect,
             label = "With placeholder",
             placeholder = "With placeholder",
+            modifier = Modifier.padding(top = 10.dp)
         )
         EmeraldTextField(
             state = textStateError.value,
             onValueChange = onValueChangedError,
             label = "With error message",
             maxLength = 10,
-            helperText = "With max length"
+            helperText = "With max length",
+            modifier = Modifier.padding(top = 10.dp)
         )
     }
 }
