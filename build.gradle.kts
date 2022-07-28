bc732a73529615b6175f42e1c5d789b289bc177b
Jhoan Gamarra <jhoangamarraurzola@gmail.com>
2022-07-28 15:59:56 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,9 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath(BuildPlugins.android)
-        classpath(BuildPlugins.kotlin)
-    }
 }
 
 plugins {
-
-    /**
-     * You should use `apply false` in the top-level build.gradle file
-     * to add a Gradle plugin as a build dependency, but not apply it to the
-     * current (root) project. You should not use `apply false` in sub-projects.
-     * For more information, see
-     * Applying external plugins with same version to subprojects.
-     */
 
     id("com.android.application") version Versions.gradlePlugin apply false
     id("com.android.library") version Versions.gradlePlugin apply false
